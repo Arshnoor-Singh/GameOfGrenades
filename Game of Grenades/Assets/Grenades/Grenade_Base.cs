4435409574d5623f463f75c5c7ef932a3b966884
KrispyKidd <talinskiy.t@digipen.edu>
2022-04-11 20:09:51 -0400

    Grenade Folder and basic grenade script

--- Game of Grenades/Assets/Grenades/Grenade_Base.cs
+++ Game of Grenades/Assets/Grenades/Grenade_Base.cs
@@ -2,21 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FuckGitHub : MonoBehaviour
+public class Grenade_Base : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        testing();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-    void testing()
-    {
-        Debug.Log("Testing");
     }
 }
