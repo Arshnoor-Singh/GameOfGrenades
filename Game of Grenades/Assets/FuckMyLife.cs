d7fab60ccb75f13f9a92a9d430774a996a687c47
José Javier Ortiz Vega <javierj.ortizvega@gmail.com>
2022-04-11 20:18:36 -0400

    testing one final test because yea why not

---
+++ Game of Grenades/Assets/FuckMyLife.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuckMyLife : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuckMyLife();
+    }
+
+    // Update is called once per frame
+    void fuckMyLife()
+    {
+        Debug.Log("Apple and Ureal are better than unity, deal with it:)");
+    }
+}
