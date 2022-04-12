292b7372ccd72ee91aa1e1aae2d9da6a1f4ddedb
José Javier Ortiz Vega <javierj.ortizvega@gmail.com>
2022-04-11 20:04:38 -0400

    testing

---
+++ Game of Grenades/Assets/testing/FuckGitHub.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuckGitHub : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        testing();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void testing()
+    {
+        Debug.Log("Testing");
+    }
+}
