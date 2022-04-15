e51968b6372e913959f63ad8e8edba2d394386c6
raperios <raed.rpar@gmail.com>
2022-04-14 23:08:13 -0400

    UI stuff added
    listeners, script to change settings and a scene to test those things

---
+++ Assets/Scripts/UI Scripts/SettingsMenu.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer mm;
+    public AudioMixer em;
+    public AudioMixer vm;
+
+    Resolution[] resolutions;
+    public Dropdown resDrop;
+    int currentRes = 0;
+
+    private bool pause;
+
+    void Start()
+    {
+        Debug.Log(resDrop.name);
+        pause = false;
+        resolutions = Screen.resolutions;
+        resDrop.ClearOptions();
+
+        List<string> options = new List<string>();
+        for(int i = 0; i < resolutions.Length; i++)
+        {
+            string op = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(op);
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentRes = i;
+            }
+        }
+        resDrop.AddOptions(options);
+        resDrop.value = currentRes;
+        resDrop.RefreshShownValue();
+    }
+
+    //Audio
+    public void setMusicVolume(float vol)
+    {
+        mm.SetFloat("volume", vol);
+    }
+
+    public void setEffectsVolume(float vol)
+    {
+        em.SetFloat("volume", vol);
+    }
+
+    public void setVoiceVolume(float vol)
+    {
+        vm.SetFloat("volume", vol);
+    }
+
+    //Graphics
+    public void setQuality(int qIndex)
+    {
+        QualitySettings.SetQualityLevel(qIndex);
+    }
+
+    public void setScreenSize(int sIndex)
+    {
+        Screen.SetResolution(resolutions[sIndex].width, resolutions[sIndex].height, false);
+        Debug.Log("bbbbbbb");
+    }
+
+    public void showSettingsMenu()
+    {
+        GameObject settingsPanel;
+        GameObject sss;
+
+    }
+}
