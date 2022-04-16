3f46c7d45d65e00c8cf4a85b2d5e3a54db2f0866
José Javier Ortiz Vega <javierj.ortizvega@gmail.com>
2022-04-16 18:32:11 -0400

    The donut and trip mine base

--- Assets/Scripts/Grenade Scripts/TheDonutScript.cs
+++ Assets/Scripts/Grenade Scripts/TheDonutScript.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [SelectionBase]
-[RequireComponent(typeof(Rigidbody))]
 [RequireComponent (typeof(TripMineBaseScript))]
 public class TheDonutScript : MonoBehaviour
 {
@@ -11,18 +10,17 @@ public class TheDonutScript : MonoBehaviour
     [Header("The donut settings")]
 
     //private to class
-    [Tooltip("Add the prefab for what will become the tripmine grenade")]
     [SerializeField] private GameObject _theDonut = null; //to call the script specific items ues the following //_theDonut.GetComponent<TripMineBaseScript>();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        slowPlayer();
+    }
+
+    //slow players based on the script's 
+    private void slowPlayer()
+    {
+        //...
     }
 }
--- Assets/Scripts/Grenade Scripts/TripMineBaseScript.cs
+++ Assets/Scripts/Grenade Scripts/TripMineBaseScript.cs
@@ -10,10 +10,33 @@
  *              ******************************
  */
 
+/*
+ *  ******************************************
+ *  ****           INSTRUCTIONS           ****
+ *  ******************************************
+ * 
+ *  Use the followint:
+ *  1. Every class that utilizes this script should hare this as a requirement
+ *        [RequireComponent(typeof(TripMineBaseScript))]
+ *    
+ *      Available functions to call for other classes:
+ *      1. Launch(Vector3 dir);
+ *      2. getTripMineName();
+ *      3. getTripMineDescription();
+ *      4. explosionImminent();
+ *      5. returnConstantVelocity();
+ *      6. gotStuckToAPlayer();
+ *      7. getCookingTime();
+ *      8. countDown();
+ *      9. getDamage();
+ *      10. getStack();
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class TripMineBaseScript : MonoBehaviour
 {
     //calling header
@@ -26,12 +49,14 @@ public class TripMineBaseScript : MonoBehaviour
     [SerializeField] public string tripMineName = null; //stores the name of the grenade
     [Tooltip("Name of the grenade")]
     [SerializeField] [Multiline] [TextArea] public string tripMineDescription = null; //description of the grenade
+    [Tooltip("Trip mine damage")]
+    [SerializeField][Range(0f, 10000f)] public float tripMineDamage = 1f; //the damage of the trip mine
     [Tooltip("Stack that is available for the player in their inventory")]
     [SerializeField] [Range(1, 10)] public int stack = 4; //stack
     [Tooltip("This accounts for the starting velocity of the grenade")]
     [SerializeField][Range(0f, 10000f)] private float _grenadeVelocity = 0f; //grenade velocity
     [Tooltip("Edit the cooking time of the grenade when it gets stuck or when it's collision box sees an enemy player")]
-    [SerializeField][Range(0f, 10000f)] private float _cookingTime = 0f; //edit how many times the grenade can bounce before it sticks to something
+    [SerializeField][Range(0f, 10000f)] public float _cookingTime = 0f; //edit how many times the grenade can bounce before it sticks to something
     [Tooltip("Has constant velocity")]
     [SerializeField] public bool constantVelocity = false; //allow the tripmine to have constant velocity until it hits something
     [Tooltip("Allows the grenade to bounce before sticking to surfaces")]
@@ -44,13 +69,22 @@ public class TripMineBaseScript : MonoBehaviour
     [SerializeField] public bool sticksToPlayers = true; //allow the grenade to stick to players
     [Tooltip("Explodes with cooking time after it's sticks to something")]
     [SerializeField] private bool _explodesWithCookingTime = true; //explodes wih cooking time
+    [Tooltip("Explode the grenade on built in explosion settings (not recommended for trip mine grenades)")]
+    [SerializeField] private bool _useBuiltInExplosionSystem = false; //use the base grenade effects for this built in simple system
 
     //class privates
-    [HideInInspector] public bool hasCooking = false; //allow the grenade to have cooking while holding it
     [HideInInspector] public bool cookingStarted = false; //starts cooking when stuck in the ground
+    [HideInInspector] private bool _isStuckToAPlayer = false; //returns true if it has been stuck to a player
 
     //class returns
     [HideInInspector] public bool _isAboutToExplode = false; //makes sure that if grenade is about to explode, another script can see this
+
+    //updates per frame
+    private void Update()
+    {
+        if(cookingStarted && _useBuiltInExplosionSystem)
+            countDown();
+    }
 
     //on collision stick or check
     private void OnCollisionEnter(Collision collision)
@@ -60,11 +94,13 @@ public class TripMineBaseScript : MonoBehaviour
     //bounce check
     private void _bounceCheck(Collision col)
     {
-        //if ((!sticksToPlayers && grenadeBounce == 0) && (col.gameObject.tag == "Player"))
-        if ((sticksToPlayers && col.gameObject.tag == "Player") || (grenadeBounce == 0)) //fix this-------------
+        if ((sticksToPlayers && col.gameObject.tag == "Player") || (grenadeBounce == 0)) //This needs an eye------------------------
         {
             this._grenadePrephab.GetComponent<Rigidbody>().isKinematic = true;
-            grenadeEffects();
+            _isStuckToAPlayer = true;
+
+            if(_useBuiltInExplosionSystem)
+                grenadeEffects();
         }
         else if (((grenadeBounce == 0) && (!canBounce)) && ((!sticksToPlayers) && (col.gameObject.tag == "Player")))
         {
@@ -87,21 +123,23 @@ public class TripMineBaseScript : MonoBehaviour
         Debug.Log("Bounces left: " + grenadeBounce);
     }
 
-    //grenade effects go here
+    //grenade effects go here--------------
     private void grenadeEffects()
     {
-        if((_cookingTime != 0) && (_explodesWithCookingTime))
+        if((_cookingTime > 0) && (_explodesWithCookingTime))
         {
-            _cookingTime -= Time.deltaTime;
+            cookingStarted = true;
         }
-        else if ((_cookingTime == 0) && (_explodesWithCookingTime))
+        else if ((_cookingTime <= 0) && (_explodesWithCookingTime))
         {
             _isAboutToExplode = true;
-            Destroy(gameObject); //add effects here
+
+            if (_useBuiltInExplosionSystem)
+                Destroy(gameObject);
         }
     }
 
-    //script return events for other scripts to utilize well---------------------------------------------------
+    //script return events for other scripts to utilize well--------------------------------------------------------------
     //launches the grenade from the movement script
     public void Launch(Vector3 dir)
     {
@@ -120,6 +158,12 @@ public class TripMineBaseScript : MonoBehaviour
         return tripMineDescription;
     }
 
+    //return the stack allowed for this grenade
+    public int getStack()
+    {
+        return stack;
+    }
+
     //tells other scripts that the grenade is about to explode and to do all of it's vfx in time
     public bool explosionImminent()
     {
@@ -129,9 +173,44 @@ public class TripMineBaseScript : MonoBehaviour
             return false;
     }
 
-    //return constant velocity 
+    //return constant velocity bool (Change in another script)
     public bool returnConstantVelocity()
     {
         return constantVelocity;
+    }
+
+    //returnn if it got stuck to a player
+    public bool gotStuckToAPlayer()
+    {
+        if (_isStuckToAPlayer)
+            return true;
+        else
+            return false;
+    }
+
+    //returns cooking time
+    public float getCookingTime()
+    {
+        return _cookingTime;
+    }
+
+    //count down to explode
+    public void countDown()
+    {
+        if (cookingStarted)
+        {
+            _cookingTime -= Time.deltaTime;
+
+            if (_useBuiltInExplosionSystem)
+                grenadeEffects();
+        }
+
+        Debug.Log("Time left before destruction: " + ((float)Mathf.Round(_cookingTime * 10f) * 0.1f));
+    }
+
+    //returns the damage of the trip mine
+    public float getDamage()
+    {
+        return tripMineDamage;
     }
 }
