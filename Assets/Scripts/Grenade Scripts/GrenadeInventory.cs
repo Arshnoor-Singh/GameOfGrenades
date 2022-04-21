db47c8ad1d24d7b3349c9611f76d21822112e3e1
Ryan Jenkins <vidipernoctem@gmail.com>
2022-04-20 22:52:32 -0400

    Added Grenade Inventory system and Grenade Item
    Added scripts for managing the players grenade inventory and accessing fields in the Grenade_Base script for Cooldown and Max Grenades.

---
+++ Assets/Scripts/Grenade Scripts/GrenadeInventory.cs
@@ -0,0 +1,157 @@
+/*
+*  Author:             Ryan Jenkins
+*  Creation Date:      4/19/2022
+*  Last Updated:       4/20/2022
+*  Description:        This script stores the grenade items collected by the player and manages which grenade is active
+*                      to pass the prefab to the controller to be thrown
+*/
+
+
+using System.Diagnostics.Contracts;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class GrenadeInventory : MonoBehaviour
+{
+    public GrenadeListScriptableObject grenadeList;
+    
+    [Range(0, 3)] private int _activeGrenadeSlot = 0; // the slot index of the currently selected grenade
+    private GrenadeItem[] _grenadeInventory = new GrenadeItem[4]; // the array of grenade prefabs the player in their inventory
+
+    // change the active inventory slot to the index provided, returns true if changed
+    public bool ChangeSlot(int slotID)
+    {
+        if (slotID >= 0 && slotID <= 3 && !_grenadeInventory[slotID].empty)
+        {
+            _activeGrenadeSlot = slotID;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    // Returns the grenade prefab of the active inventory slot
+    public GameObject GetGrenade()
+    {
+        return _grenadeInventory[_activeGrenadeSlot].GetPrefab();
+    }
+
+    // Adds a grenade prefab to the inventory in the first available slot or overrides the active slot if full
+    public void AddGrenade(GameObject newGrenade)
+    {
+        bool emptySlotFound = false;
+        for (int i = 0; i <= 3; ++i)
+        {
+            if (_grenadeInventory[i].empty)
+            {
+                _grenadeInventory[i].Init(newGrenade);
+                emptySlotFound = true;
+                break;
+            }
+        }
+
+        if (!emptySlotFound)
+        {
+            _grenadeInventory[_activeGrenadeSlot].Init(newGrenade);
+        }
+    }
+
+    // Removes the grenade from the inventory slot and returns a prefab reference or null if unable
+    public GameObject RemoveGrenade(int slotID)
+    {
+        if (slotID >= 0 && slotID <= 3 && !_grenadeInventory[slotID].empty)
+        {
+            GameObject removedGrenade = _grenadeInventory[slotID].GetPrefab();
+            _grenadeInventory[slotID].Clear();
+            return removedGrenade;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    // removes the grenade in the active inventory slot
+    public GameObject RemoveActiveGrenade()
+    {
+        return RemoveGrenade(_activeGrenadeSlot);
+    }
+}
+
+public class GrenadeItem
+{
+    private GameObject _prefab;
+    private float _timer;
+    private int _count;
+    public bool empty = true;
+
+    // set the grenade item to the provided grenade type
+    public void Init(GameObject prefab)
+    {
+        _prefab = prefab;
+        _timer = Cooldown();
+        _count = MaxGrenades();
+        empty = false;
+    }
+
+    // clear the grenade item to default values
+    public void Clear()
+    {
+        _prefab = null;
+        _timer = 0f;
+        _count = 0;
+        empty = true;
+    }
+
+    // get the prefab for this grenade type
+    public GameObject GetPrefab()
+    {
+        return _prefab;
+    }
+    
+    // get the cooldown for the grenade type
+    public float Cooldown()
+    {
+        return _prefab.GetComponent<Grenade_Base>().Cooldown;
+    }
+
+    // get the current time remaining for the grenade to replenish 1 ammo
+    public float Timer()
+    {
+        return _timer;
+    }
+    
+    // get the maximum number of grenades for this grenade type
+    public int MaxGrenades()
+    {
+        return _prefab.GetComponent<Grenade_Base>().MaxGrenades;
+    }
+
+    // get the current number of grenades possessed for this grenade type
+    public int CurrentGrenades()
+    {
+        return _count;
+    }
+    
+    // Update the items cooldown time and replenish grenades
+    public void UpdateItem()
+    {
+        // check the count against max
+        if (_count < MaxGrenades())
+        {
+            // increment the cooldown timer
+            _timer -= Time.deltaTime;
+            if (_timer <= 0)
+            {
+                ++_count;
+                _timer = Cooldown();
+            }
+        }
+        else
+        {
+            _timer = Cooldown();
+        }
+    }
+}
