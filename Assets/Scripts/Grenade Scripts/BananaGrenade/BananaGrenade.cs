8a4f764091e4f56f5b71cc19fe38d3f9e04c0d34
abhishekkd96 <abhishekkd96@gmail.com>
2022-04-27 00:42:44 -0400

    Revert "Delete BananaGrenade.cs"
    This reverts commit fa133c23005a10bae8c70424a558fae1d3fe9db1.

---
+++ Assets/Scripts/Grenade Scripts/BananaGrenade/BananaGrenade.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaGrenade : MonoBehaviour
+{
+    public GameObject smallBanana;
+    public int countBanana = 6;
+    public Vector3 SpawnRange;
+    public float maxRangeX = 5;
+    public float maxRangeZ = 5;
+    public float distanceToCenter = 1f;
+
+    public GameObject ExplosionEffect;
+    public float radious = 5f;
+    public float force = 1000f;
+    private Grenade_Base GB;
+    private bool CanExplode = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GB = transform.GetComponent<Grenade_Base>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //DEBUG
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            //Instantiate(gameObject, transform.position, Quaternion.identity);
+            Explode();
+        }
+
+        if (GB.CookingTime <= 0 && CanExplode)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Vector3 curentPos = transform.position;
+        //SpawnBananas
+        for (int i = 0; i <= countBanana; i++)
+        {
+            float randX = Random.Range(-maxRangeX, maxRangeX);
+            float randZ = Random.Range(-maxRangeZ, maxRangeZ);
+            Vector3 SpawnPos = new Vector3(curentPos.x + randX, curentPos.y + distanceToCenter, curentPos.z + randZ);
+            Instantiate(smallBanana, SpawnPos, Quaternion.identity);
+        }
+
+        //Explode
+        Debug.Log("BOOOOOOOOOM");
+        Instantiate(ExplosionEffect, transform.position, transform.rotation);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radious);
+        foreach (Collider affectedObjects in colliders)
+        {
+            Rigidbody rb = affectedObjects.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radious, 5f);
+            }
+
+            if (affectedObjects.tag == "Player")
+            {
+                //Deal damage               
+                affectedObjects.GetComponent<FragPartyCharacter>().Damage(20, GB.GrenadeOwner);
+            }
+        }
+        CanExplode = false;
+
+        Destroy(gameObject);
+
+
+    }
+}
